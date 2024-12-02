b6b83a6c3b27e97e96aeb621aad3e28615ab986e
Ray Villalobos <ray@planetoftheweb.com>
2024-12-01 23:40:13 -0500

    removed files for startup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import "@picocss/pico/css/pico.min.css";
+import "@picocss/pico/css/pico.colors.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1>Hands on React</h1>
   </StrictMode>,
 )
