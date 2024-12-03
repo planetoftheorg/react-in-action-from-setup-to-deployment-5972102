abc3be60fd4f45b8b5e3ea2a3400d83ae198104e
Ray Villalobos <ray@planetoftheweb.com>
2024-12-03 06:28:38 +0000

    finished

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+const pageTitle = document.title;
 
 function Counter() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    count && (document.title = `${pageTitle}--${count}`);
+  })
+
   return (
     <button onClick={() => setCount(count + 1)}
-    >Click Me { count }</button>
+    >Click Me { count === 0 ? ``:`${count} times` }</button>
   )
 }
 
