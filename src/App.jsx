f82030dc8fb624d19b856baa847c80a779820ce1
Ray Villalobos <ray@planetoftheweb.com>
2024-12-07 22:24:46 +0000

    Finished recording

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div className="container">
-      <Nav />
+      <Nav cast={cast} onChoice={(info) => {setMemberInfo(info)}} />
       <img src="images/group.svg" alt="StarGazers Group" />
       <h1>Meet the <i style={{ color: "SteelBlue", fontSize: '3rem' }}>{name}</i></h1>
       <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,23 @@
-function Nav() {
+function Nav({cast, onChoice}) {
   return (
     <nav className="container">
       <ul>
         <li><a href="#"><img style={{ height: '50px' }} src="images/logo_bug_stargazers.svg" alt="Stargazers Logo" /></a></li>
         <li><strong>Stargazers</strong></li>
+      </ul>
+      <ul>
+        <li>
+          <details className="dropdown">
+            <summary>Cast</summary>
+            <ul dir="rtl">
+              {cast.map(member => (
+                <li key={member.id}>
+                  <a href="#" onClick={() => { onChoice(member) }}>{member.name}</a>
+                  </li>
+                ))}
+            </ul>
+          </details>
+        </li>
       </ul>
     </nav>
   )
