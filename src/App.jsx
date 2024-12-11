00006eeab843feb3b48e39af34fe0d303c409268
Ray Villalobos <ray@planetoftheweb.com>
2024-12-11 07:24:08 +0000

    recorded

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,10 @@ function App() {
     <div className="container">
       <Nav cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
       <img src="images/group.svg" alt="StarGazers Group" />
-      <h1>Meet the {name}</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <hgroup>
+        <h1>Meet the {name}</h1>
+        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      </hgroup>
       {memberInfo &&
         <Modals
           member={memberInfo}
