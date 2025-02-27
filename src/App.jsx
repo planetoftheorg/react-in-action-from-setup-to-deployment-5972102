cf4a85be6b09070194adcd05a926aa619424a92e
Ray Villalobos <ray@planetoftheweb.com>
2025-02-27 11:42:17 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,6 @@ function App() {
       <img src="images/group.svg" alt="StarGazers Group" />
       <hgroup>
         <h1>Meet the {name}</h1>
-        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
       </hgroup>
       {memberInfo &&
         <Modals
